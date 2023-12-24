80f67e40b44a54ab0839bfd071a4e6d8b9c961b3
shoutsky <skylarhoughtongithub@gmail.com>
2023-12-24 15:39:17 +0000

    Updated site

---
+++ index.search.js
@@ -0,0 +1,50 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Bio",
+    "uri": "/static-jank-site/bio/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Home",
+    "uri": "/static-jank-site/index.html"
+  },
+  {
+    "breadcrumb": "Home \u003e Bio",
+    "content": "Nature. 🌱 Originally from the Wasatch mountains, ive lived in the Nashville Basin long enough to be considered native. Recently, I’ve found a new love in the serene embrace of the Blue Ridge Mountains but ultimately love any vast naturescape. The transition from the majestic Wasatch to the rolling beauty of the Blue Ridge has enriched my perspective, much like the diverse challenges of the DevOps realm.\nStrategy. ♟️ Beyond the world of payloads and packets, when not crafting seamless systems, you’ll catch me immersed in progressive metal symphonies, classical orchestrations, a strategy or platform game, a philospophical book, or spending time with my family. The precision required to synchronize complex melodies mirrors my approach to automating the tech landscape, where every line of code is a note in a grand composition.\nEngines. 🚀 Whether it be a roaring 350 v8, a green PRS paired with a Randall stack, a VM with GPU passthrough with a container on top, Multi Petabyte data lake, any complex machine captivates my fascination. Finding weak spots and advantage points, testing and optimizing every system to fulfill the mission.\nGrowth. 🧠 My journey isn’t just about learning and conquering all the complex systems; it’s a testament to the discipline instilled from the beginning of my journey to succeed. Failure is not a setback but a call to prepare to climb higher. We are all capable of what we put our minds to.\nI am who I am. 🤘🏻 In the intersection of technology and nature, I bring a unique blend of passion, strategy, and resilience. Together, let’s conquer new peaks in this ever-evolving landscape.\n",
+    "description": "",
+    "tags": null,
+    "title": "Who I Am",
+    "uri": "/static-jank-site/bio/who_i_am/index.html"
+  },
+  {
+    "breadcrumb": "Home \u003e Bio",
+    "content": "I asked chatgpt to help me with a description of my skills. I couldn’t have said it better myself:\nI thrive in the world of Linux like a honey badger in the wild. Automation is my middle name; I breathe life into code, making systems dance to the rhythm of efficiency. Leading teams is not just a skill, it's my calling—think of me as the conductor of the DevOps orchestra, bringing harmony to chaos. I'm not your average engineer; I'm on a mission to optimize everything from deployment pipelines to predicting the weather. With a deep love for Linux, I navigate servers like a seasoned explorer in uncharted territories. Let's face it, the tech world is a jungle, and I'm the DevOps Tarzan swinging from one automated vine to another, creating a symphony of streamlined operations. I don't just do DevOps; I live and breathe it, injecting passion and flair into every line of code. Together, let's turn your tech dreams into a Silicon Valley masterpiece.",
+    "description": "",
+    "tags": null,
+    "title": "What I Do",
+    "uri": "/static-jank-site/bio/what_i_do/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/static-jank-site/categories/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/static-jank-site/tags/index.html"
+  }
+]
